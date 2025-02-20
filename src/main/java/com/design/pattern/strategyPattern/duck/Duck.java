9889ebacdf66c54c3d08b24d6d1292c349f1573a
DongHoKim <cha11enge@hanmail.net>
2025-02-20 22:53:28 +0900

    [Strategy-Pattern] 1. 전략 패턴

--- src/main/java/com/design/pattern/strategyPattern/duck/Duck.java
+++ src/main/java/com/design/pattern/strategyPattern/duck/Duck.java
@@ -7,25 +7,24 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public void setFlyBehavior(FlyBehavior fb) {  // 동적으로 변경 가능
+        flyBehavior = fb;
+    }
+    public void setQuackBehavior(QuackBehavior qb) {  // 동적으로 변경 가능
+        quackBehavior = qb;
+    }
+
+
     public void swim() {
         System.out.println("오리가 수영합니다.");
     }
-
     public void performFly() {
         flyBehavior.fly();  // 행동 위임
     }
-
     public void performQuack() {
         quackBehavior.quack();  // 행동 위임
     }
 
-    public void setFlyBehavior(FlyBehavior fb) {  // 동적으로 변경 가능
-        flyBehavior = fb;
-    }
-
-    public void setQuackBehavior(QuackBehavior qb) {  // 동적으로 변경 가능
-        quackBehavior = qb;
-    }
 
     public abstract void display();
 }
